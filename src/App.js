44d6424f030b62a4145553a6de7955e449e51b6d
brankitocruz <brankocruz15@gmail.com>
2020-01-24 17:59:28 -0300

    funciones y clases

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Nombre(){
+  return(<h1>Brankito</h1>)
+}
+
+
+class Edad extends React.Component{
+state = {nombre: 'branko'
+}
+
+  render(){
+    return(
+      <h1>22</h1>)
+      
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <header className="App-header">
+
+        <Nombre/>
+        <Edad/>
+
+        </header>
+        
       </div>
     );
   }
